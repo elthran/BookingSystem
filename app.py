49634722c6417eced82d8185fef59d4237e478b6
elthran <jacobbrunner@gmail.com>
2018-05-25 14:11:15 +0800

    Set up the framework of the login page.

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print("Hello World")
+from flask import Flask, render_template, request, redirect, url_for
+from users import User
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form["username"]
+        return render_template('home.html')
+    return render_template('home.html', title="Login")
+
+if __name__ == '__main__':
+    app.run()
