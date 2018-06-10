8eb73095a72703e64f2ea8091b77bb6549f08f79
elthran <jacobbrunner@gmail.com>
2018-06-10 11:57:08 +0800

    Setting up setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=['booking'],
     include_package_data=True,
     install_requires=[
-        'flask',
+        'flask', 'flask-sqlalchemy', 'flask-wtf', 'flask-login'
     ],
 )
