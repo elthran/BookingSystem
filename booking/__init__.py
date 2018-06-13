095f8062538bca5f7139a8ed09dc6f45c09c7e76
Marlen Brunner <klondikemarlen@gmail.com>
2018-06-13 13:05:50 -0700

    Fix can't redirect to login page bug.

--- booking/__init__.py
+++ booking/__init__.py
@@ -67,7 +67,7 @@ from booking.models.users import User
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "signin"
+login_manager.login_view = "login"
 
 
 @login_manager.user_loader
