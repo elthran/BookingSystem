4c092e00fed731d8704ef645033220611b0896cd
elthran <jacobbrunner@gmail.com>
2018-06-15 14:37:00 +0800

    Tweaked uploads.

--- booking/functions/uploads.py
+++ booking/functions/uploads.py
@@ -1,13 +1,16 @@
 from booking import ALLOWED_EXTENSIONS
+from werkzeug.utils import secure_filename
+# Import the app itself
+from booking import app
 
 
 def allowed_file(filename):
     # Checks that the file being uploaded has a "." in it's name and that everything following the "." is a real extension
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def new_file_name(client):
-    # Turns the filename into a properly named file
-    return str(client.business_id) + "-" + str(client.id) + "-1.png"
+def new_file_name(client, extension):
+    new_name = secure_filename("%r-%r-1" % (client.business_id, client.id))
+    return app.config['UPLOAD_FOLDER'] + new_name + extension
 
 def resize_image(file):
     # Shrinks the image by 15% repeatedly until it has a length and width both less than 2500px
--- booking/routes/profiles/client.py
+++ booking/routes/profiles/client.py
@@ -1,13 +1,13 @@
 # Import the app itself
 from booking import app
 # Import flask dependencies
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, flash
 # Import session management
 from flask_login import login_required
 # Import models
 from booking.models.clients import Client
 from booking.functions.uploads import allowed_file, new_file_name, resize_image
-from werkzeug.utils import secure_filename
+
 import os
 from PIL import Image
 
@@ -20,18 +20,21 @@ def client_profile(business_id, client_id):
         # check if the post request has the file part
         if 'file' not in request.files:
             print('No file part')
+            flash("No file chosen.", "error")
             return redirect(request.url)
         file = request.files['file']
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
             print('No selected file')
+            flash("Browser can't find file.", "error")
             return redirect(request.url)
         if file and allowed_file(file.filename):
             upload = Image.open(file)   # Opens the uploaded file
             upload = resize_image(upload)   # Resizes the uploaded file
-            filename = secure_filename(new_file_name(client))
-            upload.save(app.config['UPLOAD_FOLDER'] + filename) # Saves the uploaded file
+            filename = new_file_name(client, ".png")
+            upload.save(filename, "PNG") # Saves the uploaded file
+            flash("File uploaded successfully", "notice")
     client_uploads = []
     for filename in os.listdir('booking/static/uploads'):
         if filename[0] == str(client.business_id) and filename[2] == str(client.id):
