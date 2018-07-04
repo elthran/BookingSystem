40e1b44102346eb04af58601066325d9357870a6
Marlen Brunner <klondikemarlen@gmail.com>
2018-07-04 12:57:49 -0700

    Working simplish version of AJAX/JSON data transfer via @as_json.

--- booking/routes/business/json_tests.py
+++ booking/routes/business/json_tests.py
@@ -11,11 +11,11 @@ def json():
     return render_template('json.html')
 
 
-# @as_json
 @app.route('/handle_ajax_request', methods=["GET", "POST"])
+@as_json
 def background_process_test():
     number = randint(0, 10)
     message = "Python generated this random number for you: %r" % number
     print(message)
-    # return {"number": number}
-    return jsonify(data=number)
+    return dict(data=number)
+    # return jsonify(data=number)
